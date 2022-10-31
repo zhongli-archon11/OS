f8d8f49d8648f16af6e66e7dff3571876ced7c64
Rex Lapis <91016552+zhongli-archon11@users.noreply.github.com>
2022-10-31 12:18:55 +0530

    fork()

---
+++ fork.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main()
+{
+    fork();
+    fork();
+    fork();
+    fork();
+    printf("Using fork() system call\n");
+    return 0;
+}
